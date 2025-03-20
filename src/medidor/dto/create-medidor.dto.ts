5b443d1e49580758b66802d9c1ac0d427b610be9
juan carlos chocllu <kimysheeran@gmail.com>
2025-03-19 22:58:17 -0400

    cmbios

--- src/medidor/dto/create-medidor.dto.ts
+++ src/medidor/dto/create-medidor.dto.ts
@@ -8,12 +8,14 @@ import {
 } from 'class-validator';
 import { Types } from 'mongoose';
 import { EstadoMedidorE } from '../enums/estados';
+import { Transform } from 'class-transformer';
 
 export class CreateMedidorDto {
   codigo: number;
 
   @IsString({ message: 'El número de serie debe ser un texto válido' })
   @IsNotEmpty({ message: 'El número de serie no puede estar vacío' })
+  @Transform(({ value }: { value: string }) => value.trim())
   numeroMedidor: string;
 
   @IsMongoId({ message: 'El cliente debe ser un ID de MongoDB válido' })
@@ -34,6 +36,7 @@ export class CreateMedidorDto {
 
   @IsString()
   @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim())
   direccion: string;
 
   @IsEnum(EstadoMedidorE)
